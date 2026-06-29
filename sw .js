60c580be456ad8f88fc22a2287db5bf6359408c1
vardevanyan1967-source <vardevanyan1967@mail.ru>
2026-06-29 14:57:41 +0400

    Initial commit

---
+++ sw .js
@@ -0,0 +1,12 @@
+const CACHE = 'imastun-v1';
+const ASSETS = ['/', '/index.html', '/logo.png', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
